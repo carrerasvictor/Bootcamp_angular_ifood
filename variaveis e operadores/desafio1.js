01f051b530ff0f7b4d3b51c23bdec8461427f021
Victor Carreras <ctt.victorcarreras@gmail.com>
2023-05-04 16:55:52 -0300

    Alterando

--- variaveis e operadores/desafio1.js
+++
@@ -1,9 +0,0 @@
-const precoCombustivel = 5.79;
-const gastoMedio = 12;
-const distanciaKm = 1580;
- 
-const litrosConsumidos = distanciaKm / gastoMedio;
-const valorGasto = litrosConsumidos * precoCombustivel;
-
-
-console.log(valorGasto.toFixed(2))
